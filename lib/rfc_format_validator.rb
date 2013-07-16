97d8fd4e52025e72ceff814c84d1101bb008c119
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-07-16 17:16:13 -0500

    Change internal method name.

--- lib/rfc_format_validator.rb
+++ lib/rfc_format_validator.rb
@@ -1,12 +1,12 @@
 # -*- encoding : utf-8 -*-
 class RfcFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ regexp
+    unless value =~ rfc_regexp
       object.errors[attribute] << (options[:message] || "no es un RFC válido") 
     end
   end
 
-  def regexp
+  def rfc_regexp
     if options[:force_homoclave]
       /\A[A-ZÑ&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z0-9]{3}\z/i
     else
