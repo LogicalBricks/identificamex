e4154f84fc228f1cd299494a4a2ed53e9d55b3d0
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-07-18 22:36:57 -0500

    Missing file.

---
+++ lib/identificamex/mayusculas.rb
@@ -0,0 +1,25 @@
+module Identificamex
+  module Mayusculas
+    def mayusculas(str)
+      return if str.nil?
+      str
+      .gsub(/[ÁÉÍÓÚÜáéíóúü]/, hash_vocales)
+      .upcase
+    end
+
+    def hash_vocales
+      @hash_vocales ||= {'á' => 'a',
+                         'é' => 'e',
+                         'í' => 'i',
+                         'ó' => 'o',
+                         'ú' => 'u',
+                         'ü' => 'u',
+                         'Á' => 'A',
+                         'É' => 'E',
+                         'Í' => 'I',
+                         'Ó' => 'O',
+                         'Ú' => 'U',
+                         'Ü' => 'U'}
+    end
+  end
+end
