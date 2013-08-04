6b1e90949815ed424e14715eb7706e15e483305e
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-08-03 22:12:08 -0500

    Add context method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/identificamex'
+
+class MiniTest::Spec
+  class << self
+    alias_method :context, :describe
+  end
+end
