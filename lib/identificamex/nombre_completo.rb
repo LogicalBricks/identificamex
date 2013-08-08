a8d4625208d1f5a5debd3f160a2f199a76b7ba64
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-08-08 09:47:50 -0500

    Use two kind of names.

--- lib/identificamex/nombre_completo.rb
+++ lib/identificamex/nombre_completo.rb
@@ -2,19 +2,19 @@ require_relative 'normalizador_apellido'
 
 module Identificamex
   class NombreCompleto
+    include Mayusculas
 
     def initialize(params)
-      @nombre           = normalizar_nombre(params[:nombre])
-      @primer_apellido  = normalizar_apellido(params[:primer_apellido])
-      @segundo_apellido = normalizar_apellido(params[:segundo_apellido])
+      @nombre                        = mayusculas(params[:nombre])
+      @primer_apellido               = mayusculas(params[:primer_apellido])
+      @segundo_apellido              = mayusculas(params[:segundo_apellido])
+      @nombre_simplificado           = normalizar_nombre(@nombre)
+      @primer_apellido_simplificado  = normalizar_apellido(@primer_apellido)
+      @segundo_apellido_simplificado = normalizar_apellido(@segundo_apellido)
     end
 
     def siglas
-      if siglas_apellidos_completa?
-        siglas_apellidos + primera_letra_nombre
-      else
-        siglas_apellidos + primeras_dos_letras_nombre
-      end
+      @siglas ||= PalabraInconveniente.convertir(extraer_siglas)
     end
 
     def to_s
@@ -25,27 +25,20 @@ module Identificamex
     private
     #=====================
 
-    def primera_letra_primer_apellido
-      @primer_apellido[0] if @primer_apellido.present?
+    def normalizar_nombre(nombre)
+      NormalizadorNombre.new(nombre).normalizar if nombre
     end
 
-    def primera_vocal_interna_primer_apellido
-      return unless @primer_apellido.present?
-      apellido = @primer_apellido[1..-1]
-      posicion_primera_vocal = apellido =~ /[AEIOU]/
-      apellido[posicion_primera_vocal] if posicion_primera_vocal
+    def normalizar_apellido(apellido)
+      NormalizadorApellido.new(apellido).normalizar if apellido
     end
 
-    def primera_letra_segundo_apellido
-      @segundo_apellido[0] if @segundo_apellido.present?
-    end
-
-    def primera_letra_nombre
-      @nombre[0] if @nombre.present?
-    end
-
-    def primeras_dos_letras_nombre
-      @nombre[0, 2] if @nombre.present?
+    def extraer_siglas
+      if siglas_apellidos_completa?
+        siglas_apellidos + primera_letra_nombre
+      else
+        siglas_apellidos + primeras_dos_letras_nombre
+      end
     end
 
     def siglas_apellidos
@@ -56,20 +49,44 @@ module Identificamex
       siglas_apellidos.length == 3
     end
 
-    def normalizar_nombre(nombre)
-      NormalizadorNombre.new(nombre).normalizar if nombre
-    end
-
-    def normalizar_apellido(apellido)
-      NormalizadorApellido.new(apellido).normalizar if apellido
-    end
-
     def generar_siglas_apellidos
-      return @segundo_apellido[0, 2] if @primer_apellido.nil?
-      return @primer_apellido[0, 2] if @segundo_apellido.nil?
+      return @segundo_apellido_simplificado[0, 2] if sin_primer_apellido?
+      return @primer_apellido_simplificado[0, 2] if sin_segundo_apellido?
       primera_letra_primer_apellido +
         primera_vocal_interna_primer_apellido.to_s +
         primera_letra_segundo_apellido
     end
+
+    def sin_primer_apellido?
+      @primer_apellido_simplificado.nil?
+    end
+
+    def sin_segundo_apellido?
+      @segundo_apellido_simplificado.nil?
+    end
+
+    def primera_letra_primer_apellido
+      @primer_apellido_simplificado[0] if @primer_apellido_simplificado.present?
+    end
+
+    def primera_vocal_interna_primer_apellido
+      return unless @primer_apellido_simplificado.present?
+      apellido = @primer_apellido_simplificado[1..-1]
+      posicion_primera_vocal = apellido =~ /[AEIOU]/
+      apellido[posicion_primera_vocal] if posicion_primera_vocal
+    end
+
+    def primera_letra_segundo_apellido
+      @segundo_apellido_simplificado[0] if @segundo_apellido_simplificado.present?
+    end
+
+    def primera_letra_nombre
+      @nombre_simplificado[0] if @nombre_simplificado.present?
+    end
+
+    def primeras_dos_letras_nombre
+      @nombre_simplificado[0, 2] if @nombre_simplificado.present?
+    end
+
   end
 end
