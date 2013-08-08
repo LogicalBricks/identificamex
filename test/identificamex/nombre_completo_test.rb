7e3e135274b9294f63412707c1b218720396e8e9
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-08-08 09:46:14 -0500

    Refactor to use a hash to store the name values.

--- test/identificamex/nombre_completo_test.rb
+++ test/identificamex/nombre_completo_test.rb
@@ -6,52 +6,50 @@ module Identificamex
 
     describe '#to_s' do
       it 'is (primer apellido segundo apellido y nombre)' do
-        nombre_completo = NombreCompleto.new(nombre: 'Jorge',
-                                             primer_apellido: 'Martínez',
-                                             segundo_apellido: 'López')
-        nombre_completo.to_s.must_equal 'MARTINEZ LOPEZ JORGE'
+        params = {nombre: 'José Alfredo', primer_apellido: 'Martínez', segundo_apellido: 'López'}
+        nombre_completo = NombreCompleto.new(params).to_s.must_equal 'MARTINEZ LOPEZ JOSE ALFREDO'
       end
     end
 
     describe '#siglas' do
       context 'usual cases' do
         it 'takes 2 letters from primero_apellido, 1 from segundo_apellido and 1 from the nombre' do
-          NombreCompleto.new(primer_apellido: 'Lopez', segundo_apellido: 'Sanchez', nombre: 'Ramiro')
-          .siglas.must_equal 'LOSR'
+          params = {primer_apellido: 'Lopez', segundo_apellido: 'Sanchez', nombre: 'Ramiro'}
+          NombreCompleto.new(params).siglas.must_equal 'LOSR'
         end
 
         it 'takes 2 letters from primero_apellido, 1 from segundo_apellido and 1 from the nombre' do
-          NombreCompleto.new(primer_apellido: 'Antonio', segundo_apellido: 'Sanchez', nombre: 'Ramiro')
-          .siglas.must_equal 'AOSR'
+          params = {primer_apellido: 'Antonio', segundo_apellido: 'Sanchez', nombre: 'Ramiro'}
+          NombreCompleto.new(params).siglas.must_equal 'AOSR'
         end
       end
 
       context 'special cases' do
         context 'nombre, primer_apellido and segundo_apellido have invalid words' do
           it 'takes the letters ignoring invalid words' do
-            NombreCompleto.new(primer_apellido: 'Del Angel', segundo_apellido: 'De la Rosa', nombre: 'Jose Ramiro')
-            .siglas.must_equal 'AERR'
+          params = {primer_apellido: 'Del Angel', segundo_apellido: 'De la Rosa', nombre: 'Jose Ramiro'}
+            NombreCompleto.new(params).siglas.must_equal 'AERR'
           end
         end
 
         context 'when primer_apellido has just one letter' do
           it 'takes 1 letter from primer_apellido, 1 from sengudo_apellido and 2 from nombre' do
-            NombreCompleto.new(primer_apellido: 'DE LA O', segundo_apellido: 'SANCHEZ', nombre: 'RAMIRO')
-            .siglas.must_equal 'OSRA'
+          params = {primer_apellido: 'DE LA O', segundo_apellido: 'SANCHEZ', nombre: 'RAMIRO'}
+            NombreCompleto.new(params).siglas.must_equal 'OSRA'
           end
         end
 
         context 'when primer_apellido is nil' do
           it 'takes 2 letters from segundo_apellido and 2 letters from nombre' do
-            NombreCompleto.new(primer_apellido: nil, segundo_apellido: 'SANCHEZ', nombre: 'RAMIRO')
-            .siglas.must_equal 'SARA'
+          params = {primer_apellido: nil, segundo_apellido: 'SANCHEZ', nombre: 'RAMIRO'}
+            NombreCompleto.new(params).siglas.must_equal 'SARA'
           end
         end
 
         context 'when segundo_apellido is nil' do
           it 'takes 2 letters from primer_apellido and 2 letters from nombre' do
-            NombreCompleto.new(primer_apellido: 'Sanchez', segundo_apellido: nil, nombre: 'RAMIRO')
-            .siglas.must_equal 'SARA'
+          params = {primer_apellido: 'Sanchez', segundo_apellido: nil, nombre: 'RAMIRO'}
+            NombreCompleto.new(params).siglas.must_equal 'SARA'
           end
         end
 
