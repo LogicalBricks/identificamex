37ae4be70630bdbc431bb85d50b65d1cd412e0ec
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2014-12-31 18:17:47 -0600

    Fixing closes #1 issue, the regular expression for curp was fixed

--- lib/curp_format_validator.rb
+++ lib/curp_format_validator.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 class CurpFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /\A[A-Z][AEIOUX][A-Z]{2}[0-9]{2}[0-1][0-9][0-3][0-9][MH][A-Z][BCDFGHJKLMNÑPQRSTVWXYZ]{4}[0-9A-Z][0-9]\z/i
-      object.errors[attribute] << (options[:message] || "no es una CURP válida") 
+    unless value =~ /\A[A-Z][AEIOUX][A-Z]{2}[0-9]{2}[0-1][0-9][0-3][0-9][MH][A-Z][A-Z][BCDFGHJKLMNÑPQRSTVWXYZ]{3}[0-9A-Z][0-9]\z/i
+      object.errors[attribute] << (options[:message] || "no es una CURP válida")
     end
   end
 end
